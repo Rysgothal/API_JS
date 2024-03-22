c5cf9bbce3e2eda7866e4eb38f179d7585ddfe41
Rysgothal <souzafrade2014@gmail.com>
2024-03-21 22:41:55 -0300

    Primeiro commit

---
+++ src/routes/user.router.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const { userController } = require('../controllers/user.controller');
+
+const userRouter = Router();
+
+userRouter.use(
+    '/users',
+    userRouter.get('/all', userController.getUsers),
+    userRouter.post('/add', userController.createUser),
+    userRouter.delete('/:id', userController.deleteUser),
+);
+
+module.exports = {
+    userRouter,
+}
